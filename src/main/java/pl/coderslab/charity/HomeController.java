62bd8e21d7df47f2162dedbb915048f255d03947
dominik <dominik.bin12@wp.pl>
2023-11-18 16:22:04 +0100

    sum of donations

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -4,26 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.Institution.InstitutionRepository;
-
-import javax.swing.*;
+import pl.coderslab.charity.category.CategoryRepository;
+import pl.coderslab.charity.donation.DonationRepository;
 
 
 @Controller
 public class HomeController {
 
     private final InstitutionRepository institutionRepository;
+    private final CategoryRepository categoryRepository;
+    private final DonationRepository donationRepository;
 
     @Autowired
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, CategoryRepository categoryRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.categoryRepository = categoryRepository;
+        this.donationRepository = donationRepository;
     }
 
 
     @GetMapping("/")
     public String homeAction(Model model){
         model.addAttribute("fundations", institutionRepository.findAll());
+        model.addAttribute("numberOfDonatedBags",donationRepository.sumAllDonatedBags());
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/donation/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/donation/DonationRepository.java
@@ -2,9 +2,13 @@ package pl.coderslab.charity.donation;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface DonationRepository extends JpaRepository<Donation,Long> {
+
+    @Query(value = "SELECT SUM(quantity) FROM donation", nativeQuery = true)
+    Integer sumAllDonatedBags();
 }

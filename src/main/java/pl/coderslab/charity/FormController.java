7c5f2e0ae8f1a2f52a08361a3a63f66820f2e5a0
dominik <dominik.bin12@wp.pl>
2023-11-21 21:11:02 +0100

    save donation

--- src/main/java/pl/coderslab/charity/FormController.java
+++ src/main/java/pl/coderslab/charity/FormController.java
@@ -3,24 +3,38 @@ package pl.coderslab.charity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.coderslab.charity.Institution.InstitutionRepository;
 import pl.coderslab.charity.category.CategoryRepository;
+import pl.coderslab.charity.donation.Donation;
+import pl.coderslab.charity.donation.DonationRepository;
 
 @Controller
 public class FormController {
     private final CategoryRepository categoryRepository;
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
-    public FormController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository) {
+    public FormController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.categoryRepository = categoryRepository;
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @GetMapping("/form")
     public String form(Model model) {
         model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("institutions" ,institutionRepository.findAll());
+        model.addAttribute("donation", new Donation());
         return "form";
+    }
+
+    @PostMapping("/submitForm")
+    public String submitForm(@ModelAttribute("donation") Donation donation) {
+        donationRepository.save(donation);
+
+        return "formConfirmation";
     }
 
 
